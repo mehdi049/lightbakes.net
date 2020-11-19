c970bf25032be12bd46c3de81700acf1ca064533
Mehdi Marouani <Mehdi.Marouani@vistaprint.com>
2020-11-19 13:08:36 +0100

    - init

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LightBakes.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Menu()
+        {
+            return View();
+        }
+
+        public ActionResult Product(string id)
+        {
+            return View();
+        }
+
+        public ActionResult Gallery()
+        {
+            return View();
+        }
+
+        public ActionResult Panier()
+        {
+            return View();
+        }
+    }
+}
